74185b6abebb9ffdc6dd406f03e77081c43d38f9
vandicleydson <vandicleydson@gmail.com>
2022-04-18 11:54:38 -0300

    Add Translation Lines

--- lang/overrides/portuguese-br.php
+++ lang/overrides/portuguese-br.php
@@ -1,3 +1,13 @@
 <?php
-
-
+//Front Page
+$_LANG['secureYourDomainShort'] = "Garanta Seu Domínio LOGO!";
+$_LANG['exampledomain'] = "Exemplo: www.meusite.com.br";
+$_LANG['howCanWeHelp'] = "Precisa de Ajuda?";
+$_LANG['homepage']['yourAccount'] = "Sua Conta";
+$_LANG['homepage']['manageServices'] = "Meus Produtos";
+$_LANG['homepage']['manageDomains'] = "Meus Domínios";
+$_LANG['homepage']['supportRequests'] = "Chamados";
+$_LANG['homepage']['makeAPayment'] = "Pagamentos";
+$_LANG['browseProducts'] = "Saber Mais";
+$_LANG['secureYourDomain'] = "";
+$_LANG['clientHomePanels']['productsAndServices'] = "Produtos e Serviços";
