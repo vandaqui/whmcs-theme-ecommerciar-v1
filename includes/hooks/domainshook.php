5d1bafc4bd9d0858de16873c044487d335ae6d73
Vand Aquino <vandicleydson@gmail.com>
2022-05-01 20:55:28 -0300

    Add hook

---
+++ includes/hooks/domainshook.php
@@ -0,0 +1,30 @@
+<?php
+
+if (!defined("WHMCS"))
+    die("This file cannot be accessed directly");
+
+use WHMCS\View\Menu\Item as MenuItem;
+
+//Client Area Horizontal Menu Domains > Register Domain
+add_hook('ClientAreaPrimaryNavbar', 1, function (MenuItem $primaryNavbar) {
+   if (!is_null($primaryNavbar->getChild('Domains'))) {
+       $primaryNavbar->getChild('Domains')->removeChild('Register a New Domain');
+   }
+});
+
+
+//Client Area Horizontal Menu Domains > Search Domain
+add_hook('ClientAreaPrimaryNavbar', 1, function (MenuItem $primaryNavbar) {
+   if (!is_null($primaryNavbar->getChild('Domains'))) {
+       $primaryNavbar->getChild('Domains')->removeChild('Domain Search');
+   }
+});
+
+//Client Area My Domains > Register a New Domain
+add_hook('ClientAreaSecondarySidebar', 1, function (MenuItem $secondarySidebar)
+{
+   if (!is_null($secondarySidebar->getChild('My Domains Actions'))) {
+               $secondarySidebar->getChild('My Domains Actions')
+                               ->removeChild('Register a New Domain');
+   }
+});
